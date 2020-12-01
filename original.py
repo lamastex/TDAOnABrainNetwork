8693b587bb1b213602b893ce2e9640baee1aa88b
Kieran Barber <36507821+K843Barber@users.noreply.github.com>
2020-12-01 20:05:45 +0100

    Update original.py

--- original.py
+++ original.py
@@ -1,3 +1,9 @@
+'''
+Simply reads in the original matrix and produces the m_type connection csv files.
+# Read in H5 file
+# # M_types
+
+'''
 import numpy as np
 import h5py
 import pandas as pd
@@ -8,16 +14,12 @@ from scipy.spatial import distance
 import scipy
 import random
 import os
-
-# Read in H5 file
-mc0_file = h5py.File('../../pathway_average_files/cons_locs_pathways_mc0_Column.h5', 'r')
 #############################################################################################
-#                                 Neuron Type Locations                                     #
+mc0_file = h5py.File('../../pathway_average_files/cons_locs_pathways_mc0_Column.h5', 'r')
 #############################################################################################
 populations = mc0_file.get('populations')
 connection = mc0_file.get('connectivity')
 
-# # M_types
 m_type = [
     'L1_DAC', 'L1_DLAC', 'L1_HAC', 'L1_NGC-DA', 'L1_NGC-SA', 'L1_SLAC', 'L23_BP', 'L23_BTC', 
     'L23_ChC', 'L23_DBC', 'L23_LBC', 'L23_MC', 'L23_NBC', 'L23_NGC', 'L23_PC', 'L23_SBC', 'L4_BP', 
@@ -27,10 +29,7 @@ m_type = [
     'L6_BTC', 'L6_ChC', 'L6_DBC', 'L6_IPC', 'L6_LBC', 'L6_MC', 'L6_NBC', 'L6_NGC', 'L6_SBC', 
     'L6_TPC_L1', 'L6_TPC_L4', 'L6_UTPC'
     ]
-
-# ###########################################################################################
-# #                                        All cases                                        #
-# ###########################################################################################
+###########################################################################################
 for i in m_type:
     for j in m_type:
         connect4 = pd.DataFrame(np.matrix(connection[i][j]['cMat']))
