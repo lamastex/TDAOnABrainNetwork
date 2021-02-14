7fb57db14e3c9e4f270727e59c9059727b82f47c
Kieran Barber <36507821+K843Barber@users.noreply.github.com>
2021-02-14 17:21:36 +0100

    Update compute_stats.py

--- src/compute_stats.py
+++ src/compute_stats.py
@@ -31,3 +31,4 @@ for i in idx_list:
 
 print("Number of simplices: ", pyflagser.flagser_count_unweighted(arr, directed=True))
 print("Betti counts: ", pyflagser.flagser_unweighted(arr, min_dimension=5, max_dimension=5, directed=True))
+print("Script 4 Complete!")
